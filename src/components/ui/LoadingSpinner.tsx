daa313d5806972b115c53ad6d5c5b5a9c4710f53
JaredvGraham <jgram153@icloud.com>
2025-08-09 20:56:09 -0400

    loading spinner

--- src/components/ui/LoadingSpinner.tsx
+++ src/components/ui/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const LoadingSpinner = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="h-10 w-10 animate-spin rounded-full border-b-2 border-gray-900"></div>
+      <div className=" h-20 w-20 animate-spin rounded-full border-b-2 border-gray-700 "></div>
     </div>
   );
 };
