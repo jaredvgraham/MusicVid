430377cd5a80ea509310f0c9fb50f2d6e8612139
evanwilson2123 <evanmitchellwilson@gmail.com>
2025-08-20 18:25:15 -0400

    lint

--- src/app/api/dashboard/final-renders/route.ts
+++ src/app/api/dashboard/final-renders/route.ts
@@ -8,20 +8,20 @@ import Project from "@/backend/models/Project";
 export async function GET(request: NextRequest) {
     const { userId } = await auth();
     try {
-    if (!userId) {
-        console.error("Unauthorized request");
-        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    } 
-    const user = await User.findOne({ clerkId: userId });
-    if (!user) {
-        console.error("User not found");
-        return NextResponse.json({ error: "User not found" }, { status: 404 });
+        if (!userId) {
+            console.error("Unauthorized request");
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        } 
+        const user = await User.findOne({ clerkId: userId });
+        if (!user) {
+            console.error("User not found");
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
+        }
+        const finalRenders = await Project.find({ user_id: userId, finalRender: { $exists: true }});
+        const urls = finalRenders.map((render) => render.finalRender);
+        return NextResponse.json({ urls }, { status: 200 });
+    } catch (error: unknown) {
+        const err = Utils.handleApiError(error);
+        return NextResponse.json({ error: err }, { status: err.statusCode });
     }
-    const finalRenders = await Project.find({ user_id: userId, finalRender: { $exists: true }});
-    const urls = finalRenders.map((render) => render.finalRender);
-    return NextResponse.json({ urls }, { status: 200 });
-} catch (error: unknown) {
-    const err = Utils.handleApiError(error);
-    return NextResponse.json({ error: err }, { status: err.statusCode });
-}
 }
